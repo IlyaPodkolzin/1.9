5151154f8235876a19899605508ec012ee2bd481
IlyaPodkolzin <pdk.04@mail.ru>
2020-10-07 18:42:58 +0300

    Задание 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,24 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int a[][] = new int[n][n];
+        int m = scan.nextInt();
+        int a[][] = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i + j == n - 1) {
-                    a[i][j] = 1;
-                }
-                else if (i + j < n - 1) {
-                    a[i][j] = 0;
-                }
-                else {
-                    a[i][j] = 2;
-                }
+            for (int j = 0; j < m; j++) {
+                a[i][j] = scan.nextInt();
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        System.out.println(m + " " + n);
+        for (int i = 0; i < m; i++) {
+            for (int j = n - 1; j >= 0; j--) {
                 System.out.print(a[j][i]);
-                if (j != n - 1) {
+                if (j != 0) {
                     System.out.print(" ");
                 }
             }
